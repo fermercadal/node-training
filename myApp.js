0f30567e4e5c3044502b5ffa8d9af49185451af8
Fernando Mercadal <fermercadal@outlook.com>
2020-05-26 20:14:22 -0300

    Serve static assets

--- myApp.js
+++ myApp.js
@@ -27,16 +27,13 @@ app.get("/", (req, res) => {
 
 
 /** 3) Serve an HTML file */
-
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-
-
 /** 4) Serve static assets  */
+app.use(express.static(__dirname + '/public'));
 
 
 /** 5) serve JSON on a specific route */
