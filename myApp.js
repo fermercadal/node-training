ef1457cd02e6d3088df899f51f62750771ab217f
Fernando Mercadal <fermercadal@outlook.com>
2020-05-27 19:05:14 -0300

    Something about body parser, getting ready to post requests

--- myApp.js
+++ myApp.js
@@ -112,11 +112,9 @@ app.get('/:word/echo', (req, res) => {
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-app.get('/name', (req, res) => {
-  /* 
-  var firstName = req.query.first;
-  var lastName = req.query.last; */
-  
+/*
+
+app.get('/name', (req, res) => {  
   var { first: firstName, last: lastName} = req.query;
   
   res.json({
@@ -124,9 +122,12 @@ app.get('/name', (req, res) => {
   });
 });
 
+*/
+
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 /** 12) Get data form POST  */
